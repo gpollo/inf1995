fadd05ab12634a808fc1aa44d61cf730f1f1e67a
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-02-27 22:32:19 -0500

    Extern les fonctions C en C++

--- code/include/motor.h
+++ code/include/motor.h
@@ -1,6 +1,10 @@
 #ifndef INCLUDE_MOTOR_H
 #define INCLUDE_MOTOR_H
 
+#ifdef _cplusplus
+extern "C" {
+#endif
+
 /**
  * Cette méthode initialise les moteurs du robot. À l'interne, elle l'utilise
  * les deux canaux du compteur TC0, c'est-à-dire un canal pour chaque roue.
@@ -9,5 +13,9 @@
  * compteur.
  */
 void motor_init();
+
+#ifdef _cplusplus
+}
+#endif
 
 #endif
