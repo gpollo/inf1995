11259e768d1c2d41f11b8280bd1043963753cbff
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-03-18 14:24:08 -0400

    Déplacer l'interpréteur dans son propre dossier

--- code/interpreter/main.c
+++ code/interpreter/main.c
@@ -1,23 +1,32 @@
 #include <stdlib.h>
-#include <interpreter.h>
 #include <util/delay.h>
 #include <son.h>
+#include <memory.h>
+#include <moteur.h>
 
-void** instructions[256];
+#define PAGE_SIZE 128
 
 struct loop {
     uint16_t addr;
+    uint8_t page;
     uint8_t cntr;
 };
 
-void interpreter_init(void) {
+int main() {
     /* on initialise le son */
     son_init();
-}
 
-void interpreter_execute(uint8_t* code, uint16_t len) {
+    /* on initialise la mémoire */
+    memory_init();
+
+    /* on initialise les moteurs */
+    motor_init();
+
+    /* la table de correspondance des instructions */
+    void** instructions[sizeof(uint8_t)];
+
     /* les instructions inconnues sont considérées comme des NOP */
-    for(uint16_t i = 0; i < 256; i++) instructions[i] = &&CMD_NOP;
+    for(uint16_t i = 0; i < sizeof(uint8_t); i++) instructions[i] = &&CMD_NOP;
 
     /* on initialise la table d'instructions */
     instructions[0b00000001] = &&CMD_DBT;
@@ -36,25 +45,42 @@ void interpreter_execute(uint8_t* code, uint16_t len) {
     instructions[0b11000001] = &&CMD_FBC;
     instructions[0b11111111] = &&CMD_FIN;
 
-    /* l'adresse d'exécution dans la mémoire */
-    uint16_t addr = 0;
-
     /* ce flag dit si le programme roule ou non */
     uint8_t running = 0;
 
-    /* structure de données pour une boucle */
+    /* structure de données pour la boucle */
     struct loop loop = {
         .addr = 0,
+        .page = 0,
         .cntr = 0,
     };
 
+    /* la page actuellement lue de la mémoire  */
+    uint8_t page = 0;
+
+    /* on lit une page de la mémoire */
+    uint8_t code[PAGE_SIZE];
+    memory_read_block(0, (page++)*PAGE_SIZE, PAGE_SIZE, code);
+
+    /* l'adresse d'exécution dans la mémoire */
+    uint16_t addr = 0;
+
     /* on boucle jusqu'à la fin du programme */
     while(1) {
-        /* on termine le programme si la prochaine adresse est trop grande */
-        if(addr >= len) break;
+        /* on lit une autre page si nécessaire */
+        if(addr >= PAGE_SIZE) {
+            memory_read_block(0, (page++)*PAGE_SIZE, PAGE_SIZE, code);
+            addr = 0;
+        }
 
         /* on obtient la prochaine instruction */
         uint8_t cmd = code[addr++];
+
+        /* on lit une autre page si nécessaire */
+        if(addr >= PAGE_SIZE) {
+            memory_read_block(0, (page++)*PAGE_SIZE, PAGE_SIZE, code);
+            addr = 0;
+        }
 
         /* on obtient l'adresse du jump  */
         void* jump = instructions[cmd];
@@ -75,7 +101,8 @@ CMD_DBT:
         continue;
 
 CMD_ATT:
-        /* on attend 25 fois le nombre en paramètre */
+        /* on attend 25 millisecondes fois le nombre en paramètre */
+        for(uint8_t i = code[addr++]; i > 0; i--) _delay_ms(25);
         continue;
 
 CMD_DAL:
@@ -118,16 +145,26 @@ CMD_TRG:
 
 CMD_DBC:
         /* on sauvegarde l'adresse de début de la boucle et son compteur */
-        loop.cntr = addr++;
+        loop.cntr = code[addr++];
         loop.addr = addr;
+        loop.page = page;
         continue;
 
 CMD_FBC:
-        /* on quitte la boucle ou on décremente son compteur */
-        if(loop.cntr > 0)
-            addr = loop.addr;
-        else
-            loop.cntr--;
+        /* on ne recommence pas la boucle lorsque le compteur est à zéro */
+        if(loop.cntr == 0) continue;
+
+        /* on décrémente la boucle */
+        loop.cntr--;
+
+        /* on lit à nouveau la page si nécessaire */
+        if(loop.page != page) {
+            page = loop.page;
+            memory_read_block(0, (page++)*PAGE_SIZE, PAGE_SIZE, code);
+        }
+        
+        /* on retourne à l'adresse de départ de la boucle */
+        addr = loop.addr;
         continue;
 
 CMD_FIN:
@@ -135,4 +172,6 @@ CMD_FIN:
         running = 0;
         break;
     }
+
+    while(1);
 }
