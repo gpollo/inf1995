7b694128cd5f8f64fa14cf6ef639d6ef4ff50e29
William Balea <williambalea27@gmail.com>
2018-03-20 12:14:30 -0400

    Prend en compte de la longueur du programme dans le flasher

--- code/flasher/main.c
+++ code/flasher/main.c
@@ -14,16 +14,23 @@ int main(void) {
 
     /* on commence à écrire à l'adresse zéro */
     uint16_t addr = 0;
-
+	
+	/* on obtient la taille du programme */
+	uint8_t octet1 = uart_receive();
+	uint8_t octet2 = uart_receive();
+	uint16_t taille = (octet1 << 8) | octet2; 
+	
     /* on écrit chaque octet lut à la mémoire */
-    while(1) {
+    while(addr < taille) {
         /* on lit un octet */
         uint8_t octet = uart_receive();
-
+        
         /* on l'écrit à la mémoire */
         memory_write_byte(0, addr++, octet);
 
         /* on toggle les leds */
         PORTA = (PINA == 0) ? 0xFF : 0x00;
     }
+
+    while(1);
 }
