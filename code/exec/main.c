7eeb95b4532f6a57ca30fb8946f3509c5950da32
Dalyna Pak <dapak@l3818-11.info.polymtl.ca>
2018-03-29 17:54:56 -0400

    Code sur l'identification fonctionnel

--- code/exec/main.c
+++ code/exec/main.c
@@ -3,14 +3,37 @@
 #include <memory.h>
 #include <uart.h>
 
+
+void memory_test(void) {
+    memory_init();
+
+	/* On écrit le nom du robot */
+	uart_putchar(0xf0);
+    uart_printf("FireFightingB");
+   
+    /* On écrit le numéro de l'équipe (9 octets) */
+	uart_putchar(0xf1);
+    uart_printf("6266     ");
+
+	
+	/* On écrit le groupe (1 octet) */
+	uart_putchar(0xf2);
+	
+	uart_putchar(3);
+
+
+	/* On écrit la session (4 octets)*/
+	uart_putchar(0xf3);
+    uart_printf("18-1");
+   
+ 
+    /* On écrit la couleur de la base (1 octet) */
+    uart_putchar(0xf4);
+    uart_putchar('1');
+ 
+}
+  
 int main(void) {
     uart_init();
-    memory_init();
-    uint16_t addr = 0;
-    uart_printf("\n\r");
-    while (1) {
-        uint8_t data = memory_read_byte(0, addr++);
-        uart_printf("0x%02x\t", data);
-        _delay_ms(500);
-    }
+    memory_test();
 }
