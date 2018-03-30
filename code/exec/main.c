c3cfc702a653b693920a99413cb3aba56a0a9331
DalynaPak <dalyna.pak@hotmail.fr>
2018-03-30 19:44:47 -0400

    Ajout de l'interrupt qui est fonctionnel

--- code/exec/main.c
+++ code/exec/main.c
@@ -1,12 +1,19 @@
 
-#include <util/delay.h>
-#include <memory.h>
 #include <uart.h>
+#include <memory.h>
+#include <timer.h>
+#include <adc.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <stdlib.h>
+#include <del.h>
+//#include <sensor.h>
 
 
-void memory_test(void) {
+
+void identification(void) {
     memory_init();
-
+	
 	/* On écrit le nom du robot */
 	uart_putchar(0xf0);
     uart_printf("FireFightingB");
@@ -32,8 +39,105 @@ void memory_test(void) {
     uart_putchar('1');
  
 }
+	
+	/** Compteur pour le premier callback. */
+	volatile uint8_t counter1 = 5;
+
+	/** Compteur pour le deuxième callback. */
+	volatile uint8_t counter2 = 5;	
+
+	/** Compteur pour le troisième callback. */
+	volatile uint8_t counter3 = 5;
+
+	/** Premier callback. */
+	void etat_interrupt(void);
+
+	/** Premier callback. */
+	void distance_gauche(void);
+
+	/** Premier callback. */
+	void distance_droite(void);
+
+	/** Structure pour la configuration du premier callback. */
+	struct callback callback1 = {
+		.time = 1000,
+		.func = &etat_interrupt,
+		.repeat = 1,
+	};
+
+	/** Structure pour la configuration du deuxième callback. */
+	struct callback callback2 = {
+		.time = 2500,
+		.func = &distance_gauche,
+		.repeat = 1,
+	};
+
+	/** Structure pour la configuration du troisième callback. */
+	struct callback callback3 = {
+		.time = 1500,
+		.func = &distance_droite,
+		.repeat = 1,
+	};
+
+void timer_test(void) {
+    timer_init();
+
+    /* on démarre les deux premiers timers */
+    timer_start(&callback1, &callback2);
+
+    while(callback1.repeat || callback2.repeat || callback3.repeat)
+        /* on démarre le troisième quand le premier finit */
+        if(!counter1) timer_start(&callback3, &CALLBACK_IGNORE);
+}
+
+	/* État du bouton Interrupt (1 octet) */
+void etat_interrupt(void) {
+	uart_putchar(0xf5);
+	DDRD = 0x00;    
+	 while (1) {
+	/*Si le bouton est appuyé, interrupt ___ */
+	  if (PIND & 0x04 ) {
+		_delay_ms (10);
+		uart_putchar(0x00);
+	}
+	/*S'il n'est pas appuyé, interrupt _/_ */
+	else
+	uart_putchar(0x01);
+  }
+	if(!counter1) callback1.repeat = 0;
+}
+
+	/* Distance en cm détectée par le capteur gauche (1 octet) */
+void distance_gauche(void) {
+	uart_putchar(0xf6);
+//	 while (1) {
+//       uint8_t data = memory_read_byte(0, addr++);
+//        uart_printf("0x%02x\t", data);
+//		sensor_read(&capteurs);
+//        int16_t data = distance_value(capteurs.gauche);
+//        uart_printf("%i\t", data);
+//         _delay_ms(500);
+//	    sensor_read(&capteur);
+
+ 
+//     }
+    if(!counter2) callback2.repeat = 0;
+}
+/* Distance en cm détectée par le capteur droite (1 octet) */
+void distance_droite(void) {
+	uart_putchar(0xf7);
+//	uart_putchar();
+    if(!counter3) callback3.repeat = 0;
+}
   
 int main(void) {
-    uart_init();
-    memory_test();
+	uart_init();
+    identification();
+    timer_test();
+   
+    etat_interrupt();
+// 	  adc_init();
+//    distance_gauche();
+//    distance_droite();
 }
+
