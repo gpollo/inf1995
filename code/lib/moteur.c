0b6d249640b02988e9d329567d6d18e5611a86d0
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-04-05 17:29:27 -0400

    Nettoyer moteur_ajustement()

--- code/lib/moteur.c
+++ code/lib/moteur.c
@@ -2,11 +2,12 @@
 #include <moteur.h>
 #include <util/delay.h>
 #include <sensor.h>
+#include <utils.h>
+#include <uart.h>
 
 #define DISTANCE_SOUHAITE 150
 #define CORRECTION_DOUCE 2
-#define DELAY_ROTATION90 500
-
+#define DELAY_ROTATION90 2500
 
 /* valeur par défaut du prescaler */
 #ifndef MOTEUR_PRESCALER
@@ -43,9 +44,10 @@ void moteur_init() {
 #endif
 
     /* on configure les pins de PWM sortie */
-    OC0A_DDR |= _BV(OC0A_BIT);
-    OC0B_DDR |= _BV(OC0B_BIT);
-    DDRB = 0xff;    
+    SPEED_DROITE_DDR |= _BV(SPEED_DROITE_BIT);
+    SPEED_GAUCHE_DDR |= _BV(SPEED_GAUCHE_BIT);
+    DIR_DROITE_DDR |= _BV(DIR_DROITE_BIT);
+    DIR_GAUCHE_DDR |= _BV(DIR_GAUCHE_BIT);
 }
 
 void moteur_avancer(uint8_t speed) {
@@ -76,8 +78,8 @@ void moteur_arreter() {
 
 void moteur_tourner_droite() {
     /* on tourne surplace */
-    SET_DIRECTION_RECULER(DROITE);
-    SET_DIRECTION_AVANCER(GAUCHE);
+    SET_DIRECTION_AVANCER(DROITE);
+    SET_DIRECTION_RECULER(GAUCHE);
 
     /* on active la vitesse pour roue de gauche seulement */
     SET_SPEED(DROITE, ROTATION_SPEED);
@@ -90,8 +92,8 @@ void moteur_tourner_droite() {
 
 void moteur_tourner_gauche() {
     /* on met les directions vers l'avant */
-    SET_DIRECTION_AVANCER(DROITE);
-    SET_DIRECTION_RECULER(GAUCHE);
+    SET_DIRECTION_RECULER(DROITE);
+    SET_DIRECTION_AVANCER(GAUCHE);
 
     /* on active la vitesse pour roue de droite seulement */
     SET_SPEED(DROITE, ROTATION_SPEED);
@@ -112,9 +114,9 @@ void moteur_control(struct moteur_control* control) {
 
     /* on set la direction de la roue gauche */
     if(control->gauche_avancer) {
-        SET_DIRECTION_AVANCER(DROITE);
+        SET_DIRECTION_AVANCER(GAUCHE);
     } else {
-        SET_DIRECTION_RECULER(DROITE);
+        SET_DIRECTION_RECULER(GAUCHE);
     }
 
     /* on set la vitesse des deux roues */
@@ -122,63 +124,76 @@ void moteur_control(struct moteur_control* control) {
     SET_SPEED(GAUCHE, control->gauche_speed);
 }
 
-#include <uart.h>
-
 void moteur_ajustement(struct capteurs* capteurs, uint8_t direction) {
+    /* on obtient la distance du capteurs */
+    int16_t distance;
+    if(direction == 0) {
+        distance = sensor_get_distance(capteurs->gauche);
+    } else {
+        distance = sensor_get_distance(capteurs->droite);
+    }
 
-    /* Calcul de la vitesse selon distance captés */
-    /* Chercher les valeurs des capteurs*/
-    uint16_t dist_gauche = sensor_get_distance(capteurs->gauche);
-    uint16_t dist_droite = sensor_get_distance(capteurs->droite);
+    /* on calcul l'erreur */
+    int16_t erreur = DISTANCE_SOUHAITE - distance;
 
-    /*********************************************************************** 
-     * Calcul du facteur de correction selon ces distances 
-     * Initialise les paramètres
-     * Définie la direction Gauche = 0  et Droite =1
-     * Multiplier par un facteur de correction pour corriger la trajectoire
-     ***********************************************************************/
-    int16_t erreur;
-    uint16_t speed_gauche;
-    uint16_t speed_droite;
+    /* le facteur de correction */
     int16_t correction = CORRECTION_DOUCE;
 
+    uint16_t speed_gauche;
+    uint16_t speed_droite;
+
+    /* on calcule les vitesses à envoyer aux moteurs */
+    struct moteur_control control;
     if(direction == 0) {
-        erreur = DISTANCE_SOUHAITE - dist_gauche;
         if(erreur > 0) {
+            /* on s'éloigne rapidement du mur */
             speed_gauche = ROTATION_SPEED;
             speed_droite = ROTATION_SPEED;
-            /* Gauche vers l'avant et droite vers l'arriere */
-            PORTB &=~ (1<<2);
-            PORTB |= (1<<5);
+            control.gauche_avancer = 0;
+            control.droite_avancer = 1;
         } else {
+            /* on s'approche tranquillement du mur */
             speed_gauche = ROTATION_SPEED;
             speed_droite = ROTATION_SPEED - erreur/correction;
-            /* On met les directions des deux roues vers l'avant */
-            PORTB &=~ (1<<2);
-            PORTB &=~ (1<<5);
+            control.gauche_avancer = 1;
+            control.droite_avancer = 1;
         }
     } else {
-        erreur = DISTANCE_SOUHAITE - dist_droite;
         if(erreur > 0) {
+            /* on s'éloigne rapidement du mur */
             speed_gauche = ROTATION_SPEED;
             speed_droite = ROTATION_SPEED;
-            /* Gauche recule et droite avant */
-            PORTB |= (1<<2);
-            PORTB &=~ (1<<5);
+            control.gauche_avancer = 1;
+            control.droite_avancer = 0;
         } else {
+            /* on s'approche tranquillement du mur */
             speed_gauche = ROTATION_SPEED - erreur/correction;
             speed_droite = ROTATION_SPEED;
-            /* On met les directions des deux roues vers l'avant */
-            PORTB &=~ (1<<2);
-            PORTB &=~ (1<<5);
+            control.gauche_avancer = 1;
+            control.droite_avancer = 1;
         }
     }
 
-    /* On définie la vitesse de chaque roues valeur allant de 90 à 180 */
-    OCR0A = (speed_droite > 200) ? 200 : speed_droite;
-    OCR0B = (speed_gauche > 200) ? 200 : speed_gauche;
+    /* on limite la vitesse des roues */
+    LIMIT_VALUE(speed_gauche, 200);
+    LIMIT_VALUE(speed_droite, 200);
+    control.gauche_speed = speed_gauche;
+    control.droite_speed = speed_droite;
 
-    uart_printf("%d -- %i %i -- %i %i\n\t", erreur, speed_droite, speed_gauche, OCR0A, OCR0B);
+    /* on ajuste les roues */
+    moteur_control(&control);
+
+    /* information pour debug */
+    uart_debug("%d %d -- %i %i -- %i %i / %i %i\n\r",
+        erreur,
+        distance,
+        capteurs->gauche,
+        capteurs->droite,
+        speed_gauche,
+        speed_droite,
+        control.gauche_speed,
+        control.droite_speed
+    );
 }
 
 void changement_coter(uint8_t direction) {
