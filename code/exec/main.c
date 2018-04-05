fa83ec55cb33e462b78707bfe5dc8c31e75a10c7
Simon Ritchot <simon.ritchot@polymtl.ca>
2018-04-05 13:32:12 -0400

    moteur ajustement

--- code/exec/main.c
+++ code/exec/main.c
@@ -2,22 +2,23 @@
 #include <util/delay.h>
 #include <uart.h>
 #include <sensor.h>
-
-
+#include <moteur.h>
+#include <adc.h>
 
 int main(void) {
 	DDRA = 0x00;
     uart_init();
+    moteur_init();
     adc_init();
     uart_printf("\n\r");
    
-    struct capteur capteurs;
+    struct capteurs capteurs;
 
     while (1) {
 		sensor_read(&capteurs);
-        int16_t data = distance_value(capteurs.gauche);
-        uart_printf("%i\t", data);
-        _delay_ms(500);
-
+        moteur_ajustement(&capteurs, 0);
+        int16_t data = sensor_get_distance(capteurs.gauche);
+        uart_printf("%i\n", data);
+        _delay_ms(10);
     }
 }
