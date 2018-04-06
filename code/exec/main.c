95c7782c013c89c32b53f51bb6eb65ca1a1d5cbf
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-04-06 10:46:35 -0400

    Enlever le code inutile dans exec/main.c

--- code/exec/main.c
+++ code/exec/main.c
@@ -1,23 +1,3 @@
-#include <avr/io.h>
-#include <util/delay.h>
-#include <uart.h>
-#include <sensor.h>
-
-
-
 int main(void) {
-	DDRA = 0x00;
-    uart_init();
-    adc_init();
-    uart_printf("\n\r");
-   
-    struct capteur capteurs;
-
-    while (1) {
-		sensor_read(&capteurs);
-        int16_t data = distance_value(capteurs.gauche);
-        uart_printf("%i\t", data);
-        _delay_ms(500);
-
-    }
+    while(1);
 }
