5785f16e4bbc3f7a3d0b98e48fe860c0aa3fa7dc
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-04-07 16:06:56 -0400

    Formattage

--- code/exec/main.c
+++ code/exec/main.c
@@ -38,108 +38,105 @@
 #define LED_OFF 0x03
 
 void nom_robot(void) {
-    /* On écrit le nom du robot */
+    /* on écrit le nom du robot (13 octets) */
     uart_putchar(MSG_NUMERO_SECTION);
     uart_printf(NOM_ROBOT);
 }
 
 void numero_equipe (void) {
-    /* On écrit le numéro de l'équipe (9 octets) */
+    /* on écrit le numéro de l'équipe (9 octets) */
     uart_putchar(MSG_NUMERO_EQUIPE);
     uart_printf(NUMERO_EQUIPE);
 }
 	
 void numero_section(void) {
-    /* On écrit la section (1 octet) */
+    /* on écrit la section (1 octet) */
     uart_putchar(MSG_NUMERO_SECTION);
     uart_putchar(NUMERO_SECTION);
 }
 
 void session(void) {
-    /* On écrit la session (4 octets)*/
+    /* on écrit la session (4 octets)*/
     uart_putchar(MSG_SESSION);
     uart_printf(SESSION);
 } 
  
 void couleur_base(void) {
-    /* On écrit la couleur de la base (1 octet) */
+    /* on écrit la couleur de la base (1 octet) */
     uart_putchar(MSG_COULEUR_BASE);
     uart_putchar(COULEUR_BASE);
 }
 
 void interruption_init(void) {
-	
-    /* Il bloque toutes les interruptions */
+    /* on désactive les interruptions */
     cli();
 	
     /* on active l'interruption INT0 */
     EIMSK |= (1 << INT0);
 
-    /* on configure sensiblité de l'interruption aux changements  */
+    /* on configure sensiblité de l'interruption aux changements */
     EICRA |= ISC00;
     EICRA |= ISC01;
     
-	/* Il permet de recevoir à nouveau des interruptions*/
+	/* on active les interruptions */
     sei();
 }
 
-/* On assume que le bouton est relâché (0x01) */
+/* par défaut, on assume que le bouton est relâché */
 volatile uint8_t bouton = BUTTON_RELEASED;
     
 ISR(INT0_vect) {
-    
-    /* Il bloque toutes les interruptions */
+    /* on désactive les interruptions */
     cli();
     
-    /*Il permet de vérifier si le bouton est enfoncé (0x00) ou relâché */
+    /* on détermine l'état du bouton */
     bouton = (bouton == BUTTON_PRESSED) ? 1 : 0;
     
-    /* État du bouton Interrupt (1 octet) */
+    /* on écrit l'état du bouton (1 octet) */
     uart_putchar(MSG_ETAT_INTERRUPT);
     uart_putchar(bouton);
     
-    /* Le delai du anti-rebond lorsque le bouton est appuyé */
+    /* on applique un délais pour l'anti-rebond */
     _delay_ms(DELAY_BOUNCE);
     
-    /* La fonction est finie et l'Interrupt Flag est réinitialisé à 1   */
+    /* on réactive cet interruption */
     EIFR |= (1 << INTF0);
     
-    /* Il permet de recevoir à nouveau des interruptions*/
+    /* on active les interruptions */
     sei();
 }
 
 
 void distance_gauche(void) {
-    /* Distance en cm détectée par le capteur gauche (1 octet) */
+    /* distance en cm détectée par le capteur gauche (1 octet) */
 }
 
 
 void distance_droite(void) {
-    /* Distance en cm détectée par le capteur droite (1 octet) */
+    /* distance en cm détectée par le capteur droite (1 octet) */
 }
 
 
 void couleur_del(uint8_t couleur) {
-    switch (couleur){
-        /*Si on reçoit la donnée 0, la del est éteint */
+    switch (couleur) {
 	case MSG_DEL_ETEINT:
+        /* on ferme la DEL */
 	    del_off(LED_OFF);
 	    break;
-       	/*Si on reçoit la donnée 1, la del est verte */
 	case MSG_DEL_VERTE:
+        /* on met la DEL verte */
 	    del_green();
 	    break;
-	/*Si on reçoit la donnée 2, la del est rouge */
 	case MSG_DEL_ROUGE:
+        /* on met la DEL rouge */
 	    del_red();
 	    break;
     }
 }
 
 void requete_info(uint8_t identification) {
-    
     if(identification == MSG_ENVOIE_INFO) {
-        /* Le robot envoye les infos d'identification au logiciel */
+        /* le robot envoye les infos d'identification au logiciel */
         nom_robot();
         numero_equipe();
         numero_section();
@@ -149,26 +146,25 @@ void requete_info(uint8_t identification) {
 }
 
 void listen(void) {
-   
-    /*Selon les données reçues */
+    /* selon les données reçues */
     while(1) {
         switch(uart_receive()) {
 	    case MSG_VITESSE_GAUCHE: 
-	        /*Le robot exécutera une rotation à la roue gauche */
-		moteur_controller_gauche(uart_receive());
-		break;
+	        /* le robot exécutera une rotation à la roue gauche */
+		    moteur_controller_gauche(uart_receive());
+		    break;
 	    case MSG_VITESSE_DROITE:
-		/*Le robot exécutera une rotation à la roue droite */
-		moteur_controller_droite(uart_receive());
-		break;
+		    /* le robot exécutera une rotation à la roue droite */
+		    moteur_controller_droite(uart_receive());
+		    break;
 	    case MSG_COULEUR_DEL:
-		/*Le robot changera la couleur de sa del libre */
-		couleur_del(uart_receive());
-		break;
+		    /* le robot changera la couleur de sa del libre */
+		    couleur_del(uart_receive());
+		    break;
 	    case MSG_REQUETE_INFO:
-		/*Le robot envoie ses informations sur le logiciel */
-		requete_info(uart_receive());
-		break;
+		    /* le robot envoie ses informations sur le logiciel */
+		    requete_info(uart_receive());
+		    break;
         }
     }
 }
