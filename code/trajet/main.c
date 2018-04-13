ebf282fbfc0ac02632773536219c5a34da571106
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-04-13 12:54:15 -0400

    Ajouter le programme qui switch entre les deux programmes

---
+++ code/trajet/main.c
@@ -0,0 +1,22 @@
+#include <memory.h>
+
+int main(void) {
+    /* on a besoins de la mémoire */
+    memory_init();
+
+    /* on lit le premier octet */
+    uint8_t byte = memory_read_byte(0, 0x0);
+
+    /* on flip l'octet */
+    byte = (byte == 0) ? 0x1 : 0x0;
+
+    /* on reécrit l'octet en mémoire */
+    memory_write_byte(0, 0x0, byte);
+
+    /* on exécute le bon programme selon le bon octet */
+    if(byte) {
+        /* TODO: exécuter le trajet */
+    } else {
+        /* TODO: exécuter le robodiag*/
+    }
+}
